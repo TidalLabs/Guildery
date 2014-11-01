73b5166f33a2eaaba2cc285197107e936ca2fccc
bkanber <burak@burakkanber.com>
2014-11-01 17:19:45 -0400

    Refactor Game::parse method to use Router.

--- app/classes/Game.php
+++ app/classes/Game.php
@@ -1,79 +1,66 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: bkanber
  * Date: 11/1/14
  * Time: 11:57 AM
  */
-
 class Game
 {
 
-	public static $actions = [
-		[
-			'tokens' => ['create', 'guild', 'word'],
-			'method' => 'createGuild'
-		],
-		[
-			'tokens' => ['join', 'guild', 'word'],
-			'method' => 'joinGuild'
-		],
-		[
-			'tokens' => ['show', 'guild', 'word'],
-			'method' => 'showGuild'
-		],
-		[
-			'tokens' => ['create', 'member', 'word'],
-			'method' => 'createMember'
-		],
-		[
-			'tokens' => ['describe', 'guild', 'word'],
-			'method' => 'describeGuild'
-		]
-	];
-
 	public static function parse($str)
 	{
-		$lexer = new CommandLexer();
-		$stream = $lexer->lex($str);
-		$tokens = [];
+		$str        = strtolower($str);
+		$container  = new \Illuminate\Container\Container();
+		$dispatcher = new \Illuminate\Events\Dispatcher($container);
+		$router     = new \Illuminate\Routing\Router($dispatcher, $container);
 
-		foreach ($stream as $token) {
-			$tokens[] = $token->getType();
-		}
-		array_pop($tokens);
+		$router->get("basic", function () {
+			echo "Basic!";
+		});
 
-		foreach (self::$actions as $action) {
-			if ($action['tokens'] === $tokens) {
-				return self::$action['method']($stream);
-			}
-		}
+		$router->get("create member {member}", function ($memberName) {
+			echo Game::createMember($memberName);
+		});
+
+		$router->get("create guild {guild}", function ($guildName) {
+			echo Game::createGuild($guildName);
+		});
+
+		$router->get("join guild {guild}", function ($guildName) {
+			echo Game::joinGuild($guildName);
+		});
+
+		$router->get("{describe} guild {guild}", function ($describe, $guildName) {
+			echo Game::describeGuild($guildName);
+		})->where('describe', '(describe|show|tell me about)');
+
+
+		$request    = \Illuminate\Http\Request::create($str);
+		$routeToRun = $router->dispatchToRoute($request);
 	}
 
-	public static function createGuild($stream)
+	public static function createGuild($guildName)
 	{
-		$guildName = $stream->get(2)->getValue();
 		Guild::create(['name' => $guildName]);
 	}
 
-	public static function createMember($stream)
+	public static function createMember($memberName)
 	{
-		$memberName = $stream->get(2)->getValue();
 		Member::create(['name' => $memberName]);
 	}
 
-	public static function joinGuild($stream)
+	public static function joinGuild($guildName)
 	{
 		// Todo: enforce $member
 		$member = Member::find(1);
-		$guildName = $stream->get(2)->getValue();
-		$guild = Guild::where('name', '=', $guildName)->firstOrFail();
+		$guild  = Guild::where('name', '=', $guildName)->firstOrFail();
 		$guild->members()->attach($member);
 	}
 
-	public static function describeGuild($stream)
+	public static function describeGuild($guildName)
 	{
-		$guildName = $stream->get(2)->getValue();
 		$guild = Guild::where('name', '=', $guildName)->firstOrFail();
 		return $guild->describe();
 	}
--- app/models/Guild.php
+++ app/models/Guild.php
@@ -29,14 +29,14 @@ class Guild extends Eloquent
 	public function describe()
 	{
 		$output = "The {$this->name} Guild was formed {$this->howOld()} in the age of the Oyster.\n"
-			. "The Guild boasts " . count($this->members()) . "member and has completed X quests.";
+			. "The Guild boasts " . count($this->members()) . " member and has completed X quests.";
 		// Todo: count quests.
 		return $output;
 	}
 
 	public function howOld()
 	{
-		return Carbon::now()->subWeeks($this->created_at)->diffForHumans();
+		return str_replace('before', 'ago', $this->created_at->diffForHumans(Carbon::now()));
 	}
 
 	/**
